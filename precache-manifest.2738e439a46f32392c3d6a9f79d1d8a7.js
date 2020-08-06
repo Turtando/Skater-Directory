9a4c9556e1853a3dcd9a461c90fda07019e63c36
Chris <christopheraturton@gmail.com>
2020-08-05 23:48:43 -0400

    Updates

---
+++ precache-manifest.2738e439a46f32392c3d6a9f79d1d8a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d2c7b94464eb4f2086c",
+    "url": "/Skater-Directory/static/css/main.3c3d68e7.chunk.css"
+  },
+  {
+    "revision": "2d2c7b94464eb4f2086c",
+    "url": "/Skater-Directory/static/js/main.cbc3e438.chunk.js"
+  },
+  {
+    "revision": "31df70b53e49238faa49",
+    "url": "/Skater-Directory/static/js/runtime~main.89b65792.js"
+  },
+  {
+    "revision": "170b62df80e38f78dd87",
+    "url": "/Skater-Directory/static/js/2.25783549.chunk.js"
+  },
+  {
+    "revision": "b768e2347a7a53df3e0a5981500104e6",
+    "url": "/Skater-Directory/static/media/423562.b768e234.png"
+  },
+  {
+    "revision": "b137b64322330fbbae3da65a194661a3",
+    "url": "/Skater-Directory/index.html"
+  }
+];
